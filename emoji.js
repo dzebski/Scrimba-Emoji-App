747a5e4fb3eebf31c8e9e8e6b3980c780e86591d
Oleksandr Dzebovski <dzebovski@gmail.com>
2022-06-11 15:05:21 +0300

    Initialising repo with emoji web app
    Following Scrimba tutors I do create a git repo locally for en Emoji App

---
+++ emoji.js
@@ -0,0 +1,65 @@
+
+// *** Properties *** 
+
+const myEmojis = ['💩','🔫','🤡']
+const emojiContainer = document.getElementById('emojiContainer')
+
+const emojiInput = document.getElementById('emoji-input')
+
+// *** Renders *** 
+
+renderEmoji(myEmojis)
+
+// *** Methods *** 
+
+function renderEmoji(eArray) {
+
+    emojiInput.value = ""
+    emojiContainer.innerHTML = ""
+
+    eArray.forEach (element => {
+        const createSpan = document.createElement('span')
+        createSpan.textContent = element
+        emojiContainer.append(createSpan)
+    })
+}
+
+// *** Buttons *** 
+
+// Push Button
+document.getElementById('push-btn').addEventListener('click', function() {
+    console.log('Push:Clicked')
+
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        renderEmoji(myEmojis)
+    }
+    else { console.log('Input is Empty') }
+})
+
+// Unshift Button
+document.getElementById('unshift-btn').addEventListener('click', function() {
+    console.log('Unshift:Clicked')
+    
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        renderEmoji(myEmojis)
+    }
+    else { console.log('Input is Empty') }
+})
+
+// Pop Button
+document.getElementById('pop-btn').addEventListener('click', function() {
+    console.log('Pop:Clicked')
+
+    myEmojis.pop()
+    renderEmoji(myEmojis)
+})
+
+// Shift Button
+document.getElementById('shift-btn').addEventListener('click', function() {
+    console.log('Shift:Clicked')
+
+    myEmojis.shift()
+    renderEmoji(myEmojis)
+})
